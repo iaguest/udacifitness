5baea682493408ee3036d4be86c9d88ec210d42b
ianguest <iag.1.apps@gmail.com>
2021-06-23 11:36:55 +0100

    feat: Implement calendar and tieing this into existing code

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 import reducer from './reducers'
 import { white } from './utils/colors'
+import History from './components/History';
 
 export default class App extends React.Component {
   render() {
@@ -14,7 +15,8 @@ export default class App extends React.Component {
           style={[styles.container,
                  { padding: Platform.OS === 'ios' ? 50 : 30 }]}
         >
-          <AddEntry />
+          <View style={{height:20}} />
+          <History />
         </View>
       </Provider>
     );
