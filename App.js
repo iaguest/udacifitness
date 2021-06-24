709ac6d64b60d5c3336f97fb0b3e11ec64f90ccf
ianguest <iag.1.apps@gmail.com>
2021-06-24 11:18:48 +0100

    chore: Set things up to use react-navigation on ios and android

--- App.js
+++ App.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import AddEntry from './components/AddEntry';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 import reducer from './reducers'
-import { white } from './utils/colors'
+import { purple, white } from './utils/colors'
 import History from './components/History';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View
-          style={[styles.container,
-                 { padding: Platform.OS === 'ios' ? 50 : 30 }]}
-        >
-          <View style={{height:20}} />
-          <History />
-        </View>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="History" component={History} />
+            <Tab.Screen name="Add Entry" component={AddEntry} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </Provider>
     );
   }
