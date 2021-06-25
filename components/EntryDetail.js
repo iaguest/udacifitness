cb4bdec8ea520d93824908913d19ecc0fa856a2f
ianguest <iag.1.apps@gmail.com>
2021-06-25 10:53:57 +0100

    feat: Show MetricCard in EntryDetail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,22 +1,42 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import { white } from '../utils/colors'
+import MetricCard from './MetricCard'
 
 class EntryDetail extends React.Component {
   setTitle = (entryId) => {
     this.props.navigation.setOptions({
-      title:`${entryId.slice(0, 4)}-${entryId.slice(5, 7)}-${entryId.slice(8)}`}
+      title:`${entryId.slice(5, 7)}-${entryId.slice(8)}-${entryId.slice(0, 4)}`}
     );
   }
   componentDidMount(){
     this.setTitle(this.props.route.params.entryId);
   }
   render() {
+    const { metrics, entryId } = this.props;
     return (
-      <View>
-        <Text>Entry Detail</Text>
+      <View style={styles.container}>
+        <MetricCard metrics={metrics} />
       </View>
     );
   }
 }
 
-export default EntryDetail;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15,
+  }
+})
+
+function mapStateToProps(state, { route }) {
+  const { entryId } = route.params;
+  return {
+    entryId,
+    metrics: state[entryId]
+  }
+}
+
+export default connect(mapStateToProps)(EntryDetail);
